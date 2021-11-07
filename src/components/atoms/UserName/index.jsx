2aa74881772ce29fe34a4bb4ac63a17f763e6c59
Sun-Deep <sundeepp29@gmail.com>
2021-11-07 14:35:20 +0545

    atoms::user name

---
+++ src/components/atoms/UserName/index.jsx
@@ -0,0 +1,5 @@
+const UserName = ({ text }) => {
+    return <p>{text}</p>
+}
+
+export default UserName
--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,8 +1,10 @@
+import UserName from "../../components/atoms/UserName"
 
 
 const Landing = () => {
     return <div>
         hello
+        <UserName text='Sandeep Pokhrel' />
     </div>
 }
 
