8576163711268638c1a19d9867773af19069c4bf
Sun-Deep <sundeepp29@gmail.com>
2021-11-07 15:29:36 +0545

    atoms::card photo

---
+++ src/components/atoms/FeedCard/CardPhoto/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css'
+
+const CardPhoto = ({ src }) => {
+    return <img 
+        src={src}
+        className='card-photo'
+    />
+}
+
+export default CardPhoto
--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,4 +1,4 @@
-import { CommentIcon, Count } from "../../components/atoms/Reactions"
+import CardPhoto from "../../components/atoms/CardPhoto"
 import UserName from "../../components/atoms/UserName"
 
 
@@ -6,6 +6,7 @@ const Landing = () => {
     return <div>
         hello
         <UserName text='Sandeep Pokhrel' />
+        <CardPhoto src='https://i.picsum.photos/id/1026/4621/3070.jpg?hmac=OJ880cIneqAKIwHbYgkRZxQcuMgFZ4IZKJasZ5c5Wcw' />
     </div>
 }
 
